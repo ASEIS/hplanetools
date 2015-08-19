788d84119f6c6e293bf81f7225f5bc9f2cd7f499
Keli Cheng <ikcheng322@gmail.com>
2015-08-18 23:28:57 -0500

    add command line option for initializing userInput

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import numpy as np
 import os
-stat = os.stat('planedisplacements.0')
-size = stat.st_size
-print size
+import sys
+# stat = os.stat('planedisplacements.0')
+# size = stat.st_size
+# print size
+
+print sys.argv[1:]
--- userInput.py
+++ userInput.py
@@ -3,6 +3,7 @@
 # The program contians the classes that may be used by other program.
 # ==========================================
 import os
+import sys
 class Input(object):
 	fp = ""
 	plotType = ""
@@ -28,88 +29,155 @@ class Input(object):
 	colorMap = ""
 
 
-	def __init__(self):
-		self.fp = self.get_fp()
-		self.plotType = self.get_plotType()
-		self.deltaT = self.get_deltaT()
-		self.simulationTime = self.get_int('simulationTime')
-		self.alongStrike = self.get_int('alongStrike')
-		self.downDip = self.get_int('downDip')
-		self.stepAlongStrike = self.get_int('stepAlongStrike')
-		self.stepDownDip = self.get_int('stepDownDip')
-		self.magSelect, self.magnitude = self.get_mag()
-		self.scale = self.get_scale()
-		self.cumulative = self.get_cum()
-		self.snapshots = self.get_snap()
-		if self.snapshots == 'm':
-			self.numSnapshots = self.get_numsnap()
-		self.barChoice = self.get_bar()
-		if self.barChoice:
-			self.barMin, self.barMax = self.get_min_max()
+	def __init__(self, *args):
+		if len(args) <= 1:
+			if len(args) == 1: # if file is given with command
+				self.set_fp(args[0])
+
+			elif len(args) == 0:
+				self.get_fp()
+			self.get_plotType()
+			self.get_deltaT()
+			self.get_int('simulationTime')
+			self.get_int('alongStrike')
+			self.get_int('downDip')
+			self.get_int('stepAlongStrike')
+			self.get_int('stepDownDip')
+			self.get_mag()
+
+			self.scale = self.get_scale()
+			self.cumulative = self.get_cum()
+			self.snapshots = self.get_snap()
+			if self.snapshots == 'm':
+				self.numSnapshots = self.get_numsnap()
+			self.barChoice = self.get_bar()
+			if self.barChoice:
+				self.barMin, self.barMax = self.get_min_max()
+
+		elif len(args) == 9: # if parameters are given with command
+			self.set_fp(args[0])
+			self.set_plotType(args[1])
+			self.set_deltaT(args[2])
+			self.set_int_fields('simulationTime', args[3])
+			self.set_int_fields('alongStrike', args[4])
+			self.set_int_fields('downDip', args[5])
+			self.set_int_fields('stepAlongStrike', args[6])
+			self.set_int_fields('stepDownDip', args[7])
+			self.set_mag(args[8])
+			return
+
+		else:
+			print "[ERROR]: invalid parameters."
+			return
 
 	# end of __init__
+
+	def set_fp(self, filename):
+		try:
+			fp = open(filename, 'r')
+			self.fp = fp
+			return True
+		except IOError:
+			print "[ERROR]: file not found."
+			return False
+	# end of set_fp
 
 	def get_fp(self):
 		fp = ''
 		while not fp:
 			filename = raw_input("== Enter the filename of plane data: ")
-			try:
-				fp = open(filename, 'r')
-				return fp
-			except IOError:
-				print "[ERROR]: file not found."
+			fp = set_fp(filename)
 	# end of get_fp
 
-	def get_plotType(self):
+	def set_plotType(self, ptype):
 		type_list = ['a', 'v', 'd']
+		try:
+			ptype = ptype.lower()[0]
+			if ptype not in type_list:
+				print "[ERROR]: invalid plot type."
+				return False
+			else:
+				self.plotType = ptype
+				return True
+		except IndexError:
+				print "[ERROR]: invalid plot type."
+				return False
+	# end of set_plotType
+
+	def get_plotType(self):
 		ptype = ''
 		while not ptype:
 			p = raw_input("== Select displacement, velocity, or acceleration (d/v/a) for plot: ")
-			try:
-				ptype = p.lower()[0]
-				if ptype not in type_list:
-					print "[ERROR]: invalid plot type."
-					ptype = ''
-				else:
-					return ptype
-			except IndexError:
-				print "[ERROR]: invalid plot type."
+			ptype = self.set_plotType(p)
 	# end of get_plotType
+
+	def set_deltaT(self, dt):
+		try:
+			dt = float(dt)
+		except ValueError:
+			print "[ERROR]: invalid deltaT; float ONLY."
+			return False
+
+		if dt <= 0.0:
+			print "[ERROR]: invalid deltaT; positive float ONLY."
+			return False
+		self.deltaT = dt
+		return True
+	# end of set_deltaT
+
 
 	def get_deltaT(self):
 		dt = 0.0
 		while not dt:
-			d = raw_input("== Enter dt: ")
-			try:
-				dt = float(d)
-				return dt
-			except ValueError:
-				print "[ERROR]: invalid deltaT."
+			dt = raw_input("== Enter dt: ")
+			dt = self.set_deltaT(dt)
 	# end of get_deltaT
+
+	def set_int_fields(self, varName, value):
+		try:
+			value = int(value)
+			self.__dict__[varName] = value
+			return value
+
+		except ValueError:
+			print "[ERROR]: invalid value for " + varName + "."
+			return 0
+	# end of set_int_fields
+
 
 	def get_int(self, varName):
 		"""get simulationTime, alongStrike, downDip, stepAlongStrike, and stepDownDip from user."""
 		value = 0
 		while not value:
-			i = raw_input("== Enter the value of " + varName + " : ")
-			try:
-				value = int(i)
-				return value
-			except ValueError:
-				print "[ERROR]: invalid input."
+			value = raw_input("== Enter the value of " + varName + " : ")
+			value = self.set_int_fields(varName, value)
 	# end of get_int
+
+	def set_mag(self, mag):
+		magSelect = []
+		magList = ['x','y','z']
+		mag = mag.lower().strip()
+		for m in mag:
+			if m in magList:
+				magSelect.append(m)
+			elif m not in [' ', ',']:
+				print "[ERROR]: invalid axis; x/y/z or their combinations ONLY."
+				return []
+
+		if not magSelect:
+			print "[ERROR]: invalid axis; x/y/z or their combinations ONLY."
+			return []
+
+		magSelect = list(set(magSelect)) # remove duplicates
+		self.magSelect = magSelect
+		return magSelect
+	# end of set_mag
 
 	def get_mag(self):
 		magSelect = []
-		magList = ['x','y','z']
 		while not magSelect:
-			magSelect = raw_input("== Enter the axis to plot (x/y/z/xy..): ").lower().split()
-			magSelect = list(sorted(magSelect))
-			for m in magSelect:
-				if m not in magList:
-					print "[ERROR]: invalid axis."
-					magSelect = []
-					break
+			magSelect = raw_input("== Enter the axis to plot (x/y/z/xy..): ")
+			magSelect = self.set_mag(magSelect)
 
 		# decide whether or not to plot magnitude
 		magnitude = True
@@ -120,12 +188,11 @@ class Input(object):
 				m = raw_input("== Would you like to plot the magnitude? (y/n) ").lower()
 				if m:
 					if m[0] in bool_dict.keys():
-						magnitude = bool_dict[m[0]]
+						self.magnitude = bool_dict[m[0]]
 						break
 					else:
 						print "[ERROR]: invalid input."
 						m = ''
-		return magSelect, magnitude
 	# end of get_mag
 
 	def get_scale(self):
@@ -214,8 +281,6 @@ class Input(object):
 		pass
 
 
-
-
 	def check_size(self, filename):
 		"""compare the size of given file and the estimated size"""
 		stat = os.stat(filename)
@@ -228,23 +293,30 @@ class Input(object):
 	# end of check_size
 # end of input
 
-i = Input()
-# print i.fp[0]
-print i.plotType
-print i.deltaT
-print i.simulationTime
-print i.alongStrike
-print i.downDip
-print i.alongStrike
-print i.stepAlongStrike
-print i.stepDownDip
-print i.magSelect
-print i.magnitude
-print i.scale
-print i.cumulative
-print i.snapshots
-print i.numSnapshots
-print i.barChoice
-print i.barMin
-print i.barMax
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		argument = tuple(sys.argv[1:])
+		i = Input(*argument)
+	else:
+		i = Input()
+
+	print i.__dict__
+
+	# print i.plotType
+	# print i.deltaT
+	# print i.simulationTime
+	# print i.alongStrike
+	# print i.downDip
+	# print i.alongStrike
+	# print i.stepAlongStrike
+	# print i.stepDownDip
+	# print i.magSelect
+	# print i.magnitude
+	# print i.scale
+	# print i.cumulative
+	# print i.snapshots
+	# print i.numSnapshots
+	# print i.barChoice
+	# print i.barMin
+	# print i.barMax
 
